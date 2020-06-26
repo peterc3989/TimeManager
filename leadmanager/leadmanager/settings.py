67d675e542969994e99282017e80849f7e572eee
Peter <peterc3989@gmail.com>
2020-06-26 14:55:17 -0700

    add deployment settings

--- leadmanager/leadmanager/settings.py
+++ leadmanager/leadmanager/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -140,5 +139,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Configure Django App for Heroku.
-
-django_heroku.settings(locals())
