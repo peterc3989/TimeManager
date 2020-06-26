22b88388be4be0ff0c0065441f54f56bbfbc39fa
Peter <peterc3989@gmail.com>
2020-06-26 14:27:25 -0700

    add deployment settings

--- leadmanager/leadmanager/settings.py
+++ leadmanager/leadmanager/settings.py
@@ -49,6 +49,7 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -135,7 +136,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Configure Django App for Heroku.
